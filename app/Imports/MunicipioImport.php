127d9371817d138fc092f6e78c4fe393cbfe021e
Galin-art <galin_ramirez@outlook.com>
2021-01-07 20:10:05 -0600

    fase 2.5

---
+++ app/Imports/MunicipioImport.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Imports;
+
+use App\Models\Municipio;
+use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\Importable;
+use Maatwebsite\Excel\Concerns\SkipsOnError;
+use Illuminate\Support\Facades\Hash;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+
+class MunicipioImport implements ToModel, WithHeadingRow, SkipsOnError
+{
+    use Importable;
+
+    /**
+     * @param \Throwable $e
+     */
+    public function onError(\Throwable $e)
+    {
+        // Handle the exception how you'd like.
+    }
+
+
+
+
+
+
+
+
+
+    public function model(array $row)
+    {
+        return new Municipio([
+            //
+            'municipio'=>$row['municipio'],
+            'cod_postale'=>$row['cod_postale'],
+            'region_id'=>$row['region_id'],
+        ]);
+    }
+}
